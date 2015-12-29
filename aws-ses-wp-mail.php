aede652096329b926a2e6dbf89734b64cfd7ea65
Joe Hoyle <joehoyle@gmail.com>
2015-12-28 23:30:36 -0500

    DOn't use a namespace for the top level namespace so we can define wp_mail

--- aws-ses-wp-mail.php
+++ aws-ses-wp-mail.php
@@ -10,8 +10,6 @@
  * License URI:  http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-namespace AWS_SES_WP_Mail;
-
 if ( ( ! defined( 'AWS_SES_WP_MAIL_KEY' ) || ! defined( 'AWS_SES_WP_MAIL_SECRET' ) || ! defined( 'AWS_SES_WP_MAIL_REGION' ) ) && ! defined( 'AWS_SES_WP_MAIL_USE_INSTANCE_PROFILE' ) ) {
 	return;
 }
@@ -20,12 +18,12 @@ require_once dirname( __FILE__ ) . '/inc/class-ses.php';
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once dirname( __FILE__ ) . '/inc/class-wp-cli-command.php';
-	\WP_CLI::add_command( 'aws-ses', __NAMESPACE__  . '\\WP_CLI_Command' );
+	WP_CLI::add_command( 'aws-ses', 'AWS_SES_WP_Mail\\WP_CLI_Command' );
 }
 
 if ( ! function_exists( 'wp_mail' ) ) :
 function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
-	$result = SES::get_instance()->send_wp_mail( $to, $subject, $message, $headers, $attachments );
+	$result = AWS_SES_WP_Mail\SES::get_instance()->send_wp_mail( $to, $subject, $message, $headers, $attachments );
 
 	if ( is_wp_error( $result ) ) {
 		return false;
